4cb0e0d68f32235dededd3d8355d9243d69c005c
AcviferGroup <138241762+AcviferGroup@users.noreply.github.com>
2023-07-01 12:06:39 +0300

    Add files via upload

---
+++ acvifer/assets/js/home-slider.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".home-slider", {
+    loop:true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false,  
+    },
+    pagination: {
+      el: ".swiper-pagination1",
+      clickable:true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+}); 
---
+++ acvifer/assets/js/testi-slider.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".testimonial-slider", {
+
+  spaceBetween: 150,
+  loop:true,
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false, 
+  },
+
+  pagination: {
+    el: ".swiper-pagination3",
+    clickable:true,
+  },
+
+}); 
